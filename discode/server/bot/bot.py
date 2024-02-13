56911f6538b11ae75435ec74ec4926c8eda20ca6
yetimeh <senukabandara2006@gmail.com>
2024-02-13 23:11:16 +0530

    Remove token

--- discode/server/bot/bot.py
+++ discode/server/bot/bot.py
@@ -1,15 +1,17 @@
 import discord
 from discord.ext import commands
 
+from dotenv import load_dotenv
+
+load_dotenv()
+import os
+
+
 class DiscodeBot(commands.Bot):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        
-        
-    
-    
-    
+
+
 def run_bot():
     bot = DiscodeBot(command_prefix="!", intents=discord.Intents.all())
-    bot.run("MTIwNTUwMTcwMjMyOTg2MDE2Ng.Gef9Y-.taS2DlkGGO5Qp_LQZUTZcGF8OZxHvoYH_XuVjg")
-        
+    bot.run(os.getenv("TOKEN"))
