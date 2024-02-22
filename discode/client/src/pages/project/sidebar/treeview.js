0837182e21baeaedf6be283836c95f92f859de66
yetimeh <senukabandara2006@gmail.com>
2024-02-22 22:01:32 +0530

    Add search component search functionality

--- discode/client/src/pages/project/sidebar/treeview.js
+++ discode/client/src/pages/project/sidebar/treeview.js
@@ -12,6 +12,7 @@ export default function TreeView() {
     const api = new DiscodeAPI()
 
     const [data, setData] = useState({})
+    const [searchValue, setSearchValue] = useState('');
 
     useEffect(() => {
         api.fetchComponents().then((value) => setData(new Map(Object.entries(value))))
@@ -24,6 +25,19 @@ export default function TreeView() {
         event.dataTransfer.effectAllowed = 'move';
       };
 
+    const filteredData = () => {
+        if (!searchValue.trim()) {
+          return data;
+        }
+        return Array.from(data).reduce((acc, [key, value]) => {
+          const filteredValue = value.filter(item => item.toLowerCase().includes(searchValue.toLowerCase()));
+          if (filteredValue.length > 0) {
+            acc.push([key, filteredValue]);
+          }
+          return acc;
+        }, []);
+      };
+
 
     if (!data) {return <>loading...</>}
 
@@ -32,16 +46,16 @@ export default function TreeView() {
 
     <div>
 
-        <TextField  label="Search 🔎" InputLabelProps={{style: { color: 'white', opacity: '70%' }, }} inputProps={{style: { color: 'white', backgroundColor: 'rgba(127, 235, 222, 0.1)', borderRadius: '10px'}}} variant="filled" className='rounded-lg no-drag' error={false} helperText="" size="small"/>
+        <TextField  label="Search 🔎" onChange={(e) => setSearchValue(e.target.value)} value={searchValue} InputLabelProps={{style: { color: 'white', opacity: '70%' }, }} inputProps={{style: { color: 'white', backgroundColor: 'rgba(127, 235, 222, 0.1)', borderRadius: '10px'}}} variant="filled" className='rounded-lg no-drag' error={false} helperText="" size="small"/>
        
 
         <nav class="tree-nav" className='bg-lime-500/5 pt-2'>
 
-            {Array.from(data).map(([key, value]) => (
-                <details class="tree-nav__item is-expandable">
+            {searchValue === '' ? Array.from(data).map(([key, value]) => (
+                <details class="tree-nav__item is-expandable" open>
                     
                     
-                <summary class="tree-nav__item-title">{key}</summary>
+                <summary class="tree-nav__item-title" >{key}</summary>
                         
                         {value.map((item, index) => (
 
@@ -58,6 +72,26 @@ export default function TreeView() {
                         ))}
 
                 </details>
+            )) : 
+            
+            filteredData().map(([key, value]) => (
+                
+                    value.map((item, index) => (
+                        <summary
+                          key={index}
+                          className="tree-nav__item"
+                          draggable
+                          onDragStart={(event) => {
+                            onDragStart(event, item.toLowerCase())
+                            
+                          }
+                          
+                        }
+                        >{item}</summary>
+                    ))
+
+                
+            
             ))}
         
         </nav>
